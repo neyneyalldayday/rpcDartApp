32c171a5d6a462aba113bca757a4e8fdcf2539bf
neyneyalldayday <drfrankenstein123@gmail.com>
2024-04-05 15:12:28 -0500

    first commit

---
+++ r_p_s.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main(){
+  stdout.write('sup sucka you choosing Rock, Paper or Scissors? (r/p/s)  ');
+  final input = stdin.readLineSync();
+  print(input);
+  //show a prompt
+  //erad user input from console
+  //if input is a valid move shoos the ai move at random
+  //compare the player input with the ai move
+  // show the result
+  //else ifinput is "q"
+  // quit the program
+  //else invalid input 
+
+
+
+
+}
