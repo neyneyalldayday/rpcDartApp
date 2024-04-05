5a8e3ad5e2a4f97b874ca88d64608a21f5996809
neyneyalldayday <drfrankenstein123@gmail.com>
2024-04-05 15:19:16 -0500

    added a loop

--- r_p_s.dart
+++ r_p_s.dart
@@ -1,17 +1,24 @@
 import 'dart:io';
 
 void main(){
-  stdout.write('sup sucka you choosing Rock, Paper or Scissors? (r/p/s)  ');
-  final input = stdin.readLineSync();
-  print(input);
-  //show a prompt
-  //erad user input from console
-  //if input is a valid move shoos the ai move at random
-  //compare the player input with the ai move
-  // show the result
-  //else ifinput is "q"
-  // quit the program
-  //else invalid input 
+
+
+  while(true){
+    stdout.write('sup sucka you choosing Rock, Paper or Scissors? (r/p/s)  ');
+    final input = stdin.readLineSync();
+    if(input == 'r' || input == 'p' || input == 's'){
+    print('Selected $input');
+
+   } else if  (input == 'q'){
+    break;
+   } else {
+    print('invalid input!!');
+   }
+  }
+
+
+
+
 
 
 
